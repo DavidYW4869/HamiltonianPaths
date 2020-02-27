0f12317ad8b3e385bc180496dd31c33aea169d7e
DavidYW4869 <61535563+DavidYW4869@users.noreply.github.com>
2020-02-27 09:09:41 -0700

    Create HonorsContract.java

---
+++ HonorsContract.java
@@ -0,0 +1,69 @@
+import java.util.Scanner;
+
+public class HonorsContract
+{
+    public static void main(String[] args)
+    {
+        // Declare and initialize variables such as rows and columns
+        
+        int rows, columns, size, steps, xCoord, yCoord;
+        
+        // Declare Scanner object
+        Scanner input = new Scanner(System.in);
+        
+        // Ask user to type in a integer that represents number of rows
+        System.out.println("Please enter the number of rows: ");
+        
+        // Save the input into the variable rows
+        rows = input.nextInt();
+        
+        // Ask user to type in a integer that represents number of columns
+        System.out.println("Please enter the number of columns: ");
+        
+        // Save the input into the variable columns
+        columns = input.nextInt();
+        
+        // Determine if the product of rows and columns is even or odd
+        size = rows * columns;
+        
+        // If itÅfs even
+        if (size % 2 == 0)
+        {
+            // Print out the board
+            
+            //Determine the x coordinate of the origin of the checker
+            System.out.println("Please enter the x- coordinate of the origin: ");
+            
+            // Save the input into the variable xCoord
+            xCoord = input.nextInt();
+            
+            //Determine the y coordinate of the origin of the checker
+            System.out.println("Please enter the y- coordinate of the origin: ");
+            
+            // Save the input into the variable xCoord
+            yCoord = input.nextInt();
+            
+            
+            //Determine the path
+                //Can only travel up or down
+                //Cannot travel to a previous spot
+        }
+           
+        
+        // If itÅfs odd
+        else
+           //Ask the user if they allow the checker to move off the board
+                //If the user says yes
+                    //Determine the starting point
+                    //Draw out the board
+
+               //If the user says no
+               //Print: there is no hamiltonian board.
+                System.out.println("The checkerboard does not have a Hamiltonian cycle");
+
+    }
+
+}
+
+
+
